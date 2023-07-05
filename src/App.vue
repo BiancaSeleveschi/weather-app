411238883b395025904566a2fe1ef63c1eb34e8b
BiancaSeleveschi <bianca.seleveschi@gmail.com>
2023-07-05 18:06:14 +0300

    add Pressure and Humidity

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,22 @@
         autocomplete="off"
         class="mt-5 text-light"
         placeholder="Search a city"
-        v-on:keydown.enter="getCurrentPosition"  />
+        v-on:keydown.enter="getWeatherDataByCity"/>
+    <button class="btn btn-outline-secondary d-block my-4 m-auto py-2 px-4" @click="getWeatherDataByCity">Search
+    </button>
     <h1 class="fw-bold mt-5 text-light">{{ city }}</h1>
     <h5 class="text-light">{{ currentDate }}</h5>
     <div class="temp">{{ temperature }} <span>°c</span></div>
-    <h2 class=" fst-italic fw-bold text-light">Clouds</h2>
-    <p class=" fw-bold fs-4 text-light " >1°c / 7°c</p>
-
+    <h2 class="shadow w-25 m-auto my-3 fst-italic fw-bold text-dark">Humidity: {{ humidity }}%</h2>
+    <h2 class="shadow w-25 m-auto fst-italic fw-bold text-dark">Pressure: {{ pressure }} hPa</h2>
+    <div v-show="showAlert" class="overlay">
+      <transition name="fade">
+        <div class="alert alert-danger py-4"
+             role="alert">
+          We couldn't find the location you searched.
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -27,7 +36,9 @@ export default {
       image: require("@/assets/image.jpg"),
       apiKey: process.env.VUE_APP_API_KEY,
       temperature: null,
-      curr: false,
+      humidity: null,
+      pressure: null,
+      showAlert: false,
     }
   },
   mounted() {
@@ -45,43 +56,41 @@ export default {
       }
     },
     async getWeatherData(position) {
-      const { latitude, longitude } = position.coords
+      const {latitude, longitude} = position.coords
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${this.apiKey}`
-
       try {
         const response = await fetch(apiUrl)
         const data = await response.json()
         this.city = data.name
         this.temperature = Math.round(data.main.temp)
+        this.humidity = Math.round(data.main.humidity)
+        this.pressure = Math.round(data.main.pressure)
+        console.log(data.main)
       } catch (error) {
         console.log('Error:', error)
       }
     },
+    async getWeatherDataByCity() {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`;
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        this.temperature = Math.round(data.main.temp);
+        this.humidity = Math.round(data.main.humidity)
+        this.pressure = Math.round(data.main.pressure)
+      } catch (error) {
+        console.log('Error:', error);
+        this.temperature = null;
+        this.humidity = null;
+        this.pressure = null;
+        this.showAlert = true;
+        let clear = () => (this.showAlert = false)
+        if (this.showAlert) {
+          setTimeout(clear, 3000);
+        }
+      }
+    },
   },
-  //   getCurrentCity() {
-  //     navigator.geolocation.getCurrentPosition(position => {
-  //       const { latitude, longitude } = position.coords
-  //       fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=YOUR_API_KEY`)
-  //           .then(response => response.json())
-  //           .then(data => {
-  //             this.currentCity = data.results[0].components.city
-  //           })
-  //           .catch(error => console.error(error))
-  //     })
-  //   },
-  //   async searchCity() {
-  //     const apiKey = 'aeab86d76c55d95e8376ad6c3aa3b990';
-  //     const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${apiKey}`;
-  //
-  //     try {
-  //       const response = await fetch(url);
-  //       const data = await response.json();
-  //       this.weather = data;
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  // },
 }
 </script>
 
@@ -107,8 +116,30 @@ html {
   text-shadow: 2px 10px rgba(14, 1, 1, 0.81);
 }
 
+.alert {
+  position: absolute;
+  top: 35%;
+  left: 32%;
+  width: 36%;
+  height: max-content;
+  z-index: 2;
+}
+
 .temp span {
   font-weight: 400;
+}
+
+.overlay {
+  width: 100%;
+  border-bottom: solid 1px #333;
+  display: grid;
+  background-color: #a2a2a2;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
 }
 
 input {
